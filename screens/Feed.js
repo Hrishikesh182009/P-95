c07047a3c6a2eaa79800f12e82064862a22f20aa
Hrishikesh182009 <114084757+Hrishikesh182009@users.noreply.github.com>
2023-12-03 19:39:31 +0530

    Add files via upload

---
+++ screens/Feed.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text, View, Image, ImageBackground, TouchableOpacity, StyleSheet, Alert, Platform, StatusBar, SafeAreaView, Linking, ScrollView } from 'react-native';
+
+export default class Feed extends Component{
+    render(){
+        return(
+            <Text>Feed</Text>
+        )
+    }
+}
